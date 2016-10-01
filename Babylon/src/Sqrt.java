e0320fd19896a5ae3b37267cea711a3637013b02
Daniel Hug <dhug@albany.edu>
2016-10-01 19:32:20 -0400

    Created Sqrt class for my square root Babylon Method

---
+++ Babylon/src/Sqrt.java
@@ -0,0 +1,15 @@
+/*
+ * Daniel Hug
+ * Data Structures
+ * Homework 2
+ * 10/01/16
+ * This class is used to calculate the the square root of a number
+ * using the Babylon method.
+ */
+public class Sqrt {
+	
+	
+
+	
+	
+}
